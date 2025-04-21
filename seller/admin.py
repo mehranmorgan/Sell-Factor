0ef990b092dded3ef9f5ab38b2a142ee49a95765
مهران عبداللهی <mehran.morgan@yahoo.com>
2025-04-22 00:50:02 +0330

    new layout
    استفاده از template جدید

--- seller/admin.py
+++ seller/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Seller
 
-# Register your models here.
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ['name','address']
