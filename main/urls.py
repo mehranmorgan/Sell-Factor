0aa31df526117ef5d4e0c2b45bbe3097bda45dee
مهران عبداللهی <mehran.morgan@yahoo.com>
2025-04-26 23:21:48 +0330

    change template
    all page get new

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import login
 from . import views
 
 app_name = 'main'
--- product/models.py
+++ product/models.py
@@ -4,5 +4,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='products/',null=True,blank=True)
     price = models.IntegerField(null=True,blank=True)
+    qty=models.IntegerField(null=True,blank=True)
     def __str__(self):
         return self.name
